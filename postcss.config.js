2fedc25eefbdd6fb0153b438193a789cc3b9c0c2
djcadaniel <djcadaniel@gmail.com>
2022-11-06 12:31:09 -0500

    AppFoodTailwind v1

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default'
+    }
+  }
+}
